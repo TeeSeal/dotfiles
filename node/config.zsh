86c2145163c62844316df995c3f06555182f99ac
TeeSeal <teesealz@gmail.com>
2018-06-22 15:19:29 +0300

    Handle n with zplug

--- node/config.zsh
+++ node/config.zsh
@@ -1,1 +1,3 @@
-export N_PREFIX="$HOME/.n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+export N_PREFIX="$HOME/.n"
+zplug "tj/n", hook-build:"PREFIX=$N_PREFIX make install",
+[[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
