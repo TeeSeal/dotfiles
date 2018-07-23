b7bff46845bfb6d120c1146258cee5c7c944d07a
TeeSeal <teesealz@gmail.com>
2018-07-23 14:30:27 +0300

    Add git config

---
+++ git/config.zsh
@@ -0,0 +1,4 @@
+git config --global user.name TeeSeal
+git config --global user.email teesealz@gmail.com
+which nvim && git config --global core.editor nvim
+which code && git config --global merge.tool code 
