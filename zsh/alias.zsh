3f005af5268c8aab36da88acfd364aea09b337e0
TeeSeal <teesealz@gmail.com>
2019-01-29 14:02:29 +0200

    Remove common-aliases

---
+++ zsh/alias.zsh
@@ -0,0 +1,9 @@
+alias l='ls -lFh' # size,show type,human readable
+alias ll='ls -l'  # long list
+
+alias zshrc='${=EDITOR} ~/.zshrc' # Quick access to the ~/.zshrc file
+alias grep='grep --color'
+
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+alias h='history'
