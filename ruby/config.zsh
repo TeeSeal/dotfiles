dffc415a509a3a21c09b3da70b3d2432b4458218
TeeSeal <teesealz@gmail.com>
2019-02-20 16:33:59 +0200

    Change ruby config to always run rbenv init

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,3 +1,3 @@
 RBENV_HOME="$HOME/.rbenv"
 [[ :$PATH: == *":$RBENV_HOME/bin:"* ]] || PATH+=":$RBENV_HOME/bin"
-[[ :$PATH: == *":$RBENV_HOME/shims:"* ]] || eval "$(rbenv init -)"
+eval "$(rbenv init -)"
