22374e156bd2837008307151062730c8ab9b13b9
TeeSeal <teesealz@gmail.com>
2019-02-21 14:27:08 +0200

    Use asdf

---
+++ asdf/config.zsh
@@ -0,0 +1,3 @@
+export ASDF_DIR=$(brew --prefix asdf)
+. /usr/local/opt/asdf/asdf.sh
+. /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
--- ruby/config.zsh
+++
@@ -1,3 +0,0 @@
-RBENV_HOME="$HOME/.rbenv"
-[[ :$PATH: == *":$RBENV_HOME/bin:"* ]] || PATH+=":$RBENV_HOME/bin"
-eval "$(rbenv init -)"
