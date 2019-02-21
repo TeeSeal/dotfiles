fb9389dc773b122bb8a979b4c33b2d5d07c1cb1d
TeeSeal <teesealz@gmail.com>
2019-02-21 16:11:35 +0200

    Update gitignore with asdf tool-versions

--- asdf/config.zsh
+++ asdf/config.zsh
@@ -1,3 +1,3 @@
 export ASDF_DIR=$(brew --prefix asdf)
-. /usr/local/opt/asdf/asdf.sh
-. /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
+. "$ASDF_DIR/asdf.sh"
+. "$ASDF_DIR/etc/bash_completion.d/asdf.bash"
