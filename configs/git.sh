50d2bcde841a8efac420f2d6620ac45c07bd6895
Alex Hincu <teeseal@sorta.moe>
2020-08-10 10:27:14 +0300

    Regular update

--- configs/git.sh
+++ configs/git.sh
@@ -1,3 +1,3 @@
 alias gbdar="git branch --merged master | grep -v master | cut -d/ -f2- | xargs -n 1 git push --delete origin"
-alias gitclear="gcm && glum && gbdar && gbda && gp"
+alias gitclear="gcm && gbda && glum && gbdar && gbda && gp"
 alias grhh~="git reset --hard HEAD~"
