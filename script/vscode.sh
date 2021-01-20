decb247a7e2c70277f5e86f5ffd0517eb4515385
Alex Hincu <teeseal@sorta.moe>
2021-01-20 11:36:45 +0200

    Remove VSCode configs

--- script/vscode.sh
+++ script/vscode.sh
@@ -7,16 +7,3 @@ fi
 echo
 
 which code >/dev/null 2>&1 || brew cask install visual-studio-code
-
-VSCODE_USER="$HOME/Library/Application Support/Code/User"
-
-ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_USER/settings.json"
-ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_USER/keybindings.json"
-ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_USER/" # snippets is a directory
-
-echo "✅  linked config files"
-echo "🚚  installing extensions"
-
-for ext in $(cat "$DOTFILES/vscode/extensions.txt"); do
-  code --install-extension "$ext" || true
-done
