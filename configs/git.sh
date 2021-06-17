bb6902fb90350daf43fbbbaa48d8fa6e54b0d710
Alex Hincu <teeseal@sorta.moe>
2021-06-17 18:26:52 +0300

    Remove git aliases

--- configs/git.sh
+++
@@ -1,3 +0,0 @@
-alias gbdar="git branch --merged master | grep -v master | cut -d/ -f2- | xargs -n 1 git push --delete origin"
-alias gitclear="gcm && glum && gp; gbdar; gbda"
-alias grhh~="git reset --hard HEAD~"
