6afc3f54adff7396bad37f19ebc497d3124252c7
Alex Hincu <teeseal@sorta.moe>
2022-06-06 14:38:00 +0300

    regular update

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -4,6 +4,6 @@ alias ll='ls -l'  # long list
 alias zshrc='${=EDITOR} ~/.zshrc' # Quick access to the ~/.zshrc file
 alias grep='grep --color'
 
-alias fd='find . -type d -name'
+alias fdir='find . -type d -name'
 alias ff='find . -type f -name'
 alias h='history'
