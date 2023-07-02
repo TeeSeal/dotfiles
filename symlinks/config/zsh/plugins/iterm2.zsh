f2f26e0a2aa88e53ba5ac0b85ba2dd186c464d47
Alex Hincu <alex@teeseal.dev>
2023-07-02 17:18:16 +0300

    Disable iterm integration when running Warp

--- symlinks/config/zsh/plugins/iterm2.zsh
+++ symlinks/config/zsh/plugins/iterm2.zsh
@@ -1,1 +1,3 @@
-[ -f "$ZDOTDIR/.iterm2_shell_integration.zsh" ] && source "$ZDOTDIR/.iterm2_shell_integration.zsh"
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+    [ -f "$ZDOTDIR/.iterm2_shell_integration.zsh" ] && source "$ZDOTDIR/.iterm2_shell_integration.zsh"
+fi
