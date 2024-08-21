67522cc96c53078c713690f3675e700a513cb9cb
Alex Hincu <alex@teeseal.dev>
2024-08-21 18:31:28 +0300

    feat: manage mise

--- symlinks/config/zsh/plugins/python.zsh
+++ symlinks/config/zsh/plugins/python.zsh
@@ -1,17 +1,2 @@
 alias py="python3"
 alias django="py manage.py"
-alias pyvenv="source .venv/bin/activate"
-
-function activate_python_venv() {
-	if [[ -d ./.venv ]] ; then
-		VIRTUAL_ENV_DISABLE_PROMPT=true source ./.venv/bin/activate
-	elif [[ -v VIRTUAL_ENV ]] ; then
-		parentdir="$(dirname "$VIRTUAL_ENV")"
-		if [[ "$PWD"/ != "$parentdir"/* ]] ; then
-			deactivate
-		fi
-	fi
-}
-
-activate_python_venv
-chpwd_functions+=("activate_python_venv")
