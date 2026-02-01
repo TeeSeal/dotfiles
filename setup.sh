096c495c3cd5d74b168a2cb188779af3931f36b6
Alex Hincu <alex@teeseal.dev>
2026-02-02 00:24:11 +0200

    feat: allow copying via install script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env sh
-link_file() {
-	if [ -e "$2" ]; then
-		if [ "$1" -ef "$2" ]; then
-			echo "OK:  $1 already linked"
-			return 0
-		else
-			mv "$2" "$2.bak"
-			echo "WARN:  moved $2 to $2.bak"
-		fi
-	fi
 
-	ln -sf "$1" "$2"
-	echo "OK:  linked $1 to $2"
+install_file() {
+	printf "Install %s? [S]kip/[l]ink/[c]opy: " "$2"
+	read answer
+	case $answer in
+		"l" | "link" | "c" | "copy");;
+		*) return 1;;
+	esac
+
+	case $answer in
+		"l" | "link")
+			if [ -e "$2" ]; then
+				if [ "$1" -ef "$2" ]; then
+					echo "	OK: $2 already linked"
+					return 0
+				else
+					mv "$2" "$2.bak"
+					echo "	WARN: moved $2 to $2.bak"
+				fi
+			fi
+
+			mkdir -p $(dirname $FILE_PATH)
+			ln -sf "$1" "$2"
+			echo "	OK: linked $1 to $2";;
+		"c" | "copy")
+			if [ -e "$2" ]; then
+				if cmp -s "$1" "$2"; then
+					echo "	OK: $2 already exists with the same contents"
+					return 0
+				else
+					mv "$2" "$2.bak"
+					echo "	WARN: moved $2 to $2.bak"
+				fi
+			fi
+
+			cp -f "$1" "$2"
+			echo "	OK: copied $1 to $2";;
+	esac
+
+	return 0
 }
 
 prompt_yes_no() {
@@ -29,26 +56,17 @@ CONFIGS_SRC="$DOTFILES/config"
 
 for SRC in $(find $HOME_SRC -maxdepth 1 -mindepth 1); do
 	FILE_PATH="$HOME/$(basename $SRC)"
-
-	if prompt_yes_no "Link $FILE_PATH?"; then
-		link_file $SRC $FILE_PATH
-	fi
+	install_file $SRC $FILE_PATH
 done
 
 for SRC in $(find $CONFIGS_SRC -maxdepth 2 -mindepth 2); do
 	FILE_PATH="${XDG_CONFIG_HOME:-$HOME/.config}${SRC#$CONFIGS_SRC}"
-	if prompt_yes_no "Link $FILE_PATH?"; then
-		mkdir -p $(dirname $FILE_PATH)
-		link_file $SRC $FILE_PATH
-	fi
+	install_file $SRC $FILE_PATH
 done
 
 for SRC in $(find $CONFIGS_SRC -maxdepth 1 -mindepth 1 -type f); do
 	FILE_PATH="${XDG_CONFIG_HOME:-$HOME/.config}${SRC#$CONFIGS_SRC}"
-	if prompt_yes_no "Link $FILE_PATH?"; then
-		mkdir -p $(dirname $FILE_PATH)
-		link_file $SRC $FILE_PATH
-	fi
+	install_file $SRC $FILE_PATH
 done
 
 echo; echo "✨  Done"
