9374e503a0588147329bf496960dfdec435cff94
Alex Hincu <alex@teeseal.dev>
2026-02-03 02:36:38 +0200

    feat(fish): update theme

--- config/fish/conf.d/theme.fish
+++ config/fish/conf.d/theme.fish
@@ -5,7 +5,7 @@ set --global fish_color_cancel f38ba8
 set --global fish_color_command a6e3a1
 set --global fish_color_comment 7f849c
 set --global fish_color_cwd f9e2af
-set --global fish_color_cwd_root
+set --global fish_color_cwd_root f38ba8
 set --global fish_color_end fab387
 set --global fish_color_error f38ba8
 set --global fish_color_escape eba0ac
@@ -24,7 +24,7 @@ set --global fish_color_search_match --background=313244
 set --global fish_color_selection --background=313244
 set --global fish_color_status f38ba8
 set --global fish_color_user 94e2d5
-set --global fish_color_valid_path
+set --global fish_color_valid_path f5c2e7 --underline
 set --global fish_pager_color_completion cdd6f4
 set --global fish_pager_color_description 6c7086
 set --global fish_pager_color_prefix f5c2e7
